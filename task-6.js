834379151ddc395a5f2d4ea70558dad790cb7747
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-04-26 16:27:15 +0300

    123

--- task-6.js
+++ task-6.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const input = document.querySelector("input");
+const buttonCreate = document.querySelector('#controls button[data-action="create"]');
+const buttonDestroy = document.querySelector('button[data-action="destroy"]');
+const boxes = document.querySelector("#boxes");
+
+buttonCreate.addEventListener("click", handleClick)
+
+
+function handleClick(event){
+  const value = input.value;
+  if (value >= 1 && value <= 100 ) {
+    createBoxes(value);
+  }
+};
+ 
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++){
+    const newBox = document.createElement('div');
+    newBox.style.width = size + "px";
+    newBox.style.height = size + "px";
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(newBox);
+    size += 10;
+  };
+}
